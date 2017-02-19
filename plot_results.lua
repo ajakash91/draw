cf7dc0ac86e048dddc0d066353c1ea3bf0578ce2
Akash <ajakash91@gmail.com>
2017-02-18 22:29:24 -0800

    updated plot_results.lua to handle output from draw_convolution.lua

--- plot_results.lua
+++ plot_results.lua
@@ -12,12 +12,12 @@ nngraph.setDebug(true)
 x_prediction = torch.load('x_prediction')
 --x_prediction = torch.load('x_generation')
 
-x = torch.zeros(#x_prediction, x_prediction[1]:size(2), x_prediction[1]:size(3)) 
+x = torch.zeros(#x_prediction, x_prediction[1]:size(3), x_prediction[1]:size(4)) 
 x = x:cuda()
 for i = 1, x_prediction[1]:size(1) do
   for t = 1, #x_prediction do 
       x_prediction[t][i] = x_prediction[t][i]:cuda()
-      x[{{t}, {}, {}}] = x_prediction[t][i]:gt(0.5):cuda()
+      x[{{t}, {}, {}}] = x_prediction[t][i][1]:gt(0.5):cuda()
     
   end
   image.display(x)
